2157fcebd5f0b911006a38e74d00ad21a539ed29
VashaAkulka <144496131+VashaAkulka@users.noreply.github.com>
2024-01-03 02:46:10 +0300

    Delete BitwiseValuesSwap.java

--- BitwiseValuesSwap.java
+++
@@ -1,7 +0,0 @@
-public class BitwiseValuesSwap {
-    public void swap(int first, int second) {
-        first = first ^ second;
-        second = first ^ second;
-        first = first ^ second;
-    }
-}
