6d83b0e661ae91e5b0dbeb2d44e951555fa8df58
VashaAkulka <144496131+VashaAkulka@users.noreply.github.com>
2024-01-03 02:51:06 +0300

    Delete MultiplicationTable.java

--- MultiplicationTable.java
+++
@@ -1,7 +0,0 @@
-public class MultiplicationTable {
-    public void printTable(int numberTableToPrint) {
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(numberTableToPrint + " * " + i + " = " + numberTableToPrint * i);
-        }
-    }
-}
